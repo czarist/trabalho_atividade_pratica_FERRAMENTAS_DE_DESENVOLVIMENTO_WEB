af0fcdb950ce211863fb0e31cff7183a9d72d77b
Cezar  Trentin <czartrentin@gmail.com>
2021-10-21 01:15:04 -0300

    comentários

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-
+  // criação das variaveis
   elements: any = [
     {
       nome: 'Lucas Cezar Trentin',
